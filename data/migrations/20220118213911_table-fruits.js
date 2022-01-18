07ea416db1f43ccefa6c3790f7a13aa184c12e4e
RFNoC <rfnoc@rfnocs-pro-51.lan>
2022-01-18 13:55:12 -0800

    migration file build

--- data/migrations/20220118213911_table-fruits.js
+++ data/migrations/20220118213911_table-fruits.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
+exports.up = async function(knex) {
+    await knex.schema.createTable('fruits', table => {
+      // shortcut for create col 'id' which is primary key with auto-inc integers
+      table.increments() // pass the column name if you want different than 'id'
+      table.text('name', 128).unique().notNullable()
+      table.decimal('avgWeightOz').notNullable()
+      table.boolean('delicious')
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('fruits')
+  };
