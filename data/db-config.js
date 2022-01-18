08ab79aa617c9efb809de8d00c8b8ba4993b6b9d
Ladrillo <gabriel@cabrejas.net>
2022-01-18 18:58:02 +0100

    committing to "lecture"

--- data/db-config.js
+++ data/db-config.js
@@ -5,6 +5,7 @@ const configs = require('../knexfile')
 // on Heroku, process.env.NODE_ENV will be 'production'
 const env = process.env.NODE_ENV || 'development'
 
+// we need [ ] when object prop is a variable
 const configToUse = configs[env]
 
 module.exports = knex(configToUse)
