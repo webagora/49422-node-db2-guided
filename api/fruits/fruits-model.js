eba1be2ca0886859c5f6d26d63217537d4fe511f
Ladrillo <gabriel@cabrejas.net>
2022-01-18 18:58:55 +0100

    committing to "lecture"

--- api/fruits/fruits-model.js
+++ api/fruits/fruits-model.js
@@ -1,12 +1,13 @@
 const knex = require('knex')
+const db = require('../../data/db-config')
 
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './data/produce.db3'
-  },
-  useNullAsDefault: true
-});
+// const db = knex({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: './data/produce.db3'
+//   },
+//   useNullAsDefault: true
+// });
 
 function getAll() {
   return db('fruits');
