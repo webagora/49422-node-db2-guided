bb78b5cb108b51a9c638d9c08bb5e29cb56d3376
Ladrillo <gabriel@cabrejas.net>
2022-01-18 18:56:19 +0100

    committing to "lecture"

--- data/db-config.js
+++ data/db-config.js
@@ -2,4 +2,6 @@ const knex = require('./knex')
 
 const configs = require('../knexfile')
 
+// on Heroku, process.env.NODE_ENV will be 'production'
 const env = process.env.NODE_ENV || 'development'
+
