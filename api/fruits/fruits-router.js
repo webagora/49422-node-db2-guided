28d7de394364564df967437c549b0cac523c2298
RFNoC <rfnoc@rfnocs-pro-51.lan>
2022-01-18 13:01:15 -0800

    handle err.message

--- api/fruits/fruits-router.js
+++ api/fruits/fruits-router.js
@@ -1,17 +1,17 @@
 const Fruit = require('./fruits-model');
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   Fruit.getAll()
     .then(fruits => {
       res.json(fruits);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Failed to retrieve fruits' });
+      next({ message: `Failed to retrieve fruits: ${err.message}` });
     });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   Fruit.getById(req.params.id)
     .then(fruit => {
       if (fruit) {
@@ -21,18 +21,25 @@ router.get('/:id', (req, res) => {
       }
     })
     .catch(err => {
-      res.status(500).json({ message: 'Failed to retrieve fruit' });
+      next({ message: `Failed to retrieve fruit: ${err.message}` });
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   Fruit.create(req.body)
     .then(newFruitEntry => {
       res.status(201).json(newFruitEntry);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Failed to create fruit' });
+      next({ message: `Failed to create fruit: ${err.message}` });
     });
 });
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json ({
+    message: err.message,
+    stack: err.stack
+  })
+})
 
 module.exports = router;
