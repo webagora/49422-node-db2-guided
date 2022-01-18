c9b668e8eddcc876de422096175c50b216595646
Ladrillo <gabriel@cabrejas.net>
2022-01-18 19:28:17 +0100

    committing to "lecture"

--- data/migrations/20220118181017_table-fruits.js
+++ data/migrations/20220118181017_table-fruits.js
@@ -3,7 +3,7 @@ exports.up = async function(knex) {
     // shortcut for create col 'id' which is primary key with auto-inc integers
     table.increments() // pass the column name if you want different than 'id'
     table.text('name', 128).unique().notNullable()
-    table.decimal('avgWeightOz').ble()
+    table.decimal('avgWeightOz').notNullable()
     table.boolean('delicious')
   })
 };
