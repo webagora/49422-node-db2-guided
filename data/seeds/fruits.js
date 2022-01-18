9b3a6677f228ca1ce8bea554c73fb264f29b11a1
Ladrillo <gabriel@cabrejas.net>
2022-01-18 19:56:47 +0100

    committing to "lecture"

--- data/seeds/fruits.js
+++ data/seeds/fruits.js
@@ -1,11 +1,11 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   return knex('fruits').truncate()
     .then(function () {
       return knex('fruits').insert([
-        {name: 'orange', avgWeightOz: 1.5},
-        {name: 'pear', avgWeightOz: 1.5},
-        {name: 'grapes', avgWeightOz: 1.5},
+        { name: 'orange', avgWeightOz: 1.5, delicious: true, color: 'green' },
+        { name: 'pear', avgWeightOz: 2.5, delicious: false, color: 'orange' },
+        { name: 'grapes', avgWeightOz: 5, delicious: true, color: 'green' },
       ]);
     });
 };
