1905feea6520fbeec3eb29337176e0f3cf0476d3
Ladrillo <gabriel@cabrejas.net>
2022-01-18 19:23:29 +0100

    committing to "lecture"

--- data/migrations/20220118181017_table-fruits.js
+++ data/migrations/20220118181017_table-fruits.js
@@ -2,8 +2,9 @@ exports.up = async function(knex) {
   await knex.schema.createTable('fruits', table => {
     // shortcut for create col 'id' which is primary key with auto-inc integers
     table.increments() // pass the column name if you want different than 'id'
-    table.text('name')
-    table.
+    table.text('name', 128)
+    table.decimal('avgWeightOz')
+    table.boolean('delicious')
   })
 };
 
