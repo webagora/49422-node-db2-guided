9a2c7b38c5b95403b6195e0ee53489dcc3279755
Ladrillo <gabriel@cabrejas.net>
2022-01-18 19:45:57 +0100

    committing to "lecture"

--- data/migrations/20220118184345_delicious-col-fruits.js
+++ data/migrations/20220118184345_delicious-col-fruits.js
@@ -1,12 +1,12 @@
 
 exports.up = async function(knex) {
   await knex.schema.table('fruits', table => {
-    table.string('color')
+    table.string('color').defaultTo('green')
   })
 };
 
 exports.down = async function(knex) {
   await knex.schema.table('fruits', table => {
-    
+    table.dropColumn('color')
   })
 };
